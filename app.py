4563f36098ae65f5fc06963bace8e0d89c436f58
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2023-05-21 03:14:36 +0900

    1차 디자인

--- app.py
+++ app.py
@@ -1,42 +1,47 @@
-# Mongo DB사용
-from flask import Flask, render_template, request, redirect, url_for, flash
-# from flask_pymongo import PyMongo
-# from bson.objectid import ObjectId
-# from datetime import datetime
-import os
+# 필요한 패키지 임포트
+from flask import Flask, render_template, request, redirect, session
+from pymongo import MongoClient
 
+# Flask 애플리케이션 생성
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
-# static_dir = os.path.abspath('./src/static')
-# template_dir = os.path.abspath('./src/templates')
+# MongoDB 클라이언트 생성
+client = MongoClient('mongodb://localhost:27017/')
+db = client['coin_marketplace']
 
-# templates 실행
-@app.route('/')
-def layout():
-    # routes폴더로 상세경로 지정
-    return render_template('login.html')
-
-# /login 경로로 접속시 실행
+# 로그인 페이지
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # POST방식으로 접근시
     if request.method == 'POST':
-        # login.html에서 입력한 id와 pw를 받아옴
-        id = request.form['userid']
-        pw = request.form['userpw']
-        #routes를 통해 mongoDB에 접근해 id와 pw를 받아옴
-        # id와 pw가 일치할 경우
-        if id == 'admin' and pw == '1234':
-            return render_template('market.html')
-        # id와 pw가 일치하지 않을 경우
-        else:
-            return render_template('login.html')
-    # GET방식으로 접근시
-    else:
-        # login.html로 이동
-        return redirect(url_for('/'))
+        username = request.form['username']
+        password = request.form['password']
+        
+        # 로그인 인증 로직 작성
+        # 사용자 정보를 DB에서 조회하고 인증 처리
+        
+        # 인증 성공 시 세션 설정
+        session['username'] = username
+        return redirect('/coin_exchange')
+    
+    return render_template('login.html')
 
+# 코인 거래소 페이지
+@app.route('/coin_exchange')
+def coin_exchange():
+    if 'username' not in session:
+        return redirect('/login')
+    
+    # 현재 코인 가격 및 거래소 정보 조회 로직 작성
+    
+    return render_template('coin_exchange.html')
 
+# 로그아웃
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect('/login')
+
+# 애플리케이션 실행
 if __name__ == '__main__':
-    # app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
-    app.run(debug=True) # debug=True는 수정하면 바로바로 반영됨
+    app.run()
