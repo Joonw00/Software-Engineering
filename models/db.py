bd2f33942c89c6a4dcb8862f812e38762f714df8
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2023-06-08 15:10:44 +0900

    ssl 풀어놓기

--- models/db.py
+++ models/db.py
@@ -1,6 +1,7 @@
+import ssl
 import pymongo
 
-cluster = pymongo.MongoClient("mongodb+srv://joonwoo:p5628!pshp@cluster0.9hk9scr.mongodb.net/?retryWrites=true&w=majority")
+cluster = pymongo.MongoClient("mongodb+srv://joonwoo:p5628!pshp@cluster0.9hk9scr.mongodb.net/?retryWrites=true&w=majority", ssl_cert_reqs=ssl.CERT_NONE)
 DB = cluster["software_engineering"]
 # users_collection = DB["users"]
 
