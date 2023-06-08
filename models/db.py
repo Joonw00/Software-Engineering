20b1253f65843a28b0f8da93c895fbac50d60db2
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2023-06-08 15:30:00 +0900

    modified:   models/db.py

--- models/db.py
+++ models/db.py
@@ -1,6 +1,7 @@
+import certifi
 import pymongo
 
-cluster = pymongo.MongoClient("mongodb+srv://joonwoo:p5628!pshp@cluster0.9hk9scr.mongodb.net/?retryWrites=true&w=majority")
+cluster = pymongo.MongoClient("mongodb+srv://joonwoo:p5628!pshp@cluster0.9hk9scr.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=certifi.where())
 DB = cluster["software_engineering"]
 # users_collection = DB["users"]
 
